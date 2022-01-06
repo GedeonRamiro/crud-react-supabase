4a73f83c72bed8ae430f493705fdcbebedc50804
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-01-06 15:07:29 -0300

    feat: create Post

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ const App = () => {
 
   const [posts, setPosts] = useState([])
   const [post, setPost] = useState({ title: '', context: '' })
-  const { tilte, context } = post
+  const { title, context } = post
 
-  console.log(posts)
 
   const fetchPosts = async () => {
     const { data } = await supabase.from('posts').select()
     setPosts(data)
+  }
+
+  const createPost = async (e) => {
+    e.preventDefault()
+    try {
+      await supabase.from('posts').insert([
+        {title, context}
+      ]).single()
+      setPost({ title: '', context: '' })
+      fetchPosts()
+    } catch (error) {
+      console.log({error})
+    }
+
   }
 
   useEffect(() => {
@@ -21,25 +34,40 @@ const App = () => {
   return (
     <div className="App">
       <div className='container mx-auto'>
-      <div class="form-control">
-  <label class="label">
-    <span class="label-text">Primary</span>
-  </label> 
-  <input type="text" placeholder="username" class="input input-primary input-bordered" />
-</div> 
-<div class="form-control">
-  <label class="label">
-    <span class="label-text">Secondary</span>
-  </label> 
-  <input type="text" placeholder="username" class="input input-secondary input-bordered" />
-</div> 
-<div class="form-control">
-  <label class="label">
-    <span class="label-text">Accent</span>
-  </label> 
-  <input type="text" placeholder="username" class="input input-accent input-bordered" />
-</div>
-<button class="btn btn-primary">primary</button> 
+        <div className='mx-4'>
+            <h1 className='mt-20 mb-6 text-4xl font-bold'>Criar Post</h1>
+            <form onSubmit={createPost} className="form-control">
+              <label className="label">
+                <span className="label-text">Título</span>
+              </label> 
+              <input 
+                type="text" 
+                placeholder="Título do post" 
+                className="input input-accent input-bordered"
+                value={title}
+                onChange={e => setPost({...post, title: e.target.value})} 
+              />
+              <label className="label">
+                <span className="label-text">Descrição</span>
+              </label> 
+              <textarea 
+                className="h-24 textarea textarea-bordered textarea-accent" 
+                placeholder="Descrição do post"
+                value={context}
+                onChange={e => setPost({...post, context: e.target.value})}
+              />
+              <button className="mt-4 btn btn-accent">Salvar</button> 
+            </form>
+
+              {posts.map(post => (
+                <div className="my-10 shadow card shadow-green-300" key={post.id}>
+                  <div className="card-body">
+                    <h2 className="card-title">{post.title}</h2> 
+                    <p>{post.context}</p>
+                  </div>
+                </div>   
+              ))}
+        </div>
 
       </div>
     </div>
